af92c953f9f1b0a78860e66a1c6ef7ce9a18d4b3
정성우 <sanberrypie@gmail.com>
2023-07-03 17:47:05 +0900

    6장

---
+++ BikeShop/BikeShop/ProductsManagementMVVMViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeShop
+{
+    public class ProductsManagementMVVMViewModel : Notifier
+    {
+        #region Properties
+
+        private string? searchInput;
+        public string? SearchInput
+        {
+            get => searchInput;
+            set
+            {
+                searchInput = value;
+                OnPropertyChanged(nameof(SearchInput));
+                OnSearchInputChanged();
+            }
+        }
+
+        private IEnumerable<Product>? products;
+        public IEnumerable<Product>? Products
+        {
+            get => products;
+            set
+            {
+                products = value;
+                OnPropertyChanged(nameof(Products));
+            }
+        }
+
+        private Product? selectedItem;
+        public Product? SelectedItem
+        {
+            get => selectedItem;
+            set {
+                selectedItem = value;
+                OnPropertyChanged(nameof(SelectedItem));
+            }
+        }
+
+        #endregion
+
+        private ProductsFactory factory = new ProductsFactory();
+
+        public ProductsManagementMVVMViewModel() => Products = Enumerable.Empty<Product>();
+
+        private void OnSearchInputChanged()
+        {
+            SelectedItem = null;
+            Products = factory.FindProducts(SearchInput);
+        }
+
+    }
+}
